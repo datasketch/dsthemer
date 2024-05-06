656eda5713abfc2e2f62ffeb50ab6ec276655976
CamilaAchuri <camilaachury.4@gmail.com>
2024-05-06 14:47:37 -0300

    theme to json

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,3 @@
-
-`%||%` <- function (x, y){
-  if (is.empty(x))
-    return(y)
-  else if (is.null(x) || is.na(x))
-    return(y)
-  else if (class(x) == "character" && nchar(x) == 0)
-    return(y)
-  else x
-}
-
-is.empty <- function (x){
-  !as.logical(length(x))
-}
-
 file_path_sans_ext <- function (x, compression = FALSE)
 {
   if (compression)
--- tests/testthat/test-get_theme.R
+++ tests/testthat/test-get_theme.R
@@ -1,62 +1,13 @@
-test_that("Get theme", {
+test_that("call theme", {
 
-  dsthemer_list()
 
   org <- "datasketch"
-  dsthemer_get("datasketch")
-  dsthemer_list("datasketch")
+  load_dsthemer_json("datasketch")
 
-  dsthemer_get("poder")
-  dsthemer_get("poder", theme = "dark", palette = "divergent")
-  dsthemer_palettes("poder", "dark")
-  dsthemer_palette("poder", theme = "light", palette = "main")
-
-  dsthemer_get("dialogos")
-  dsthemer_get("dialogos", theme = "dark", palette = "divergent")
-
-  th <- dsthemer_get("dialogos", theme = "light")
-  expect_true(file.exists(th$logo))
-  th <- dsthemer_get("dialogos", theme = "dark")
-  expect_true(grepl("dialogos/dark.png$",th$logo))
-
-  dsthemer_palettes("dialogos", theme = "light")
-  dsthemer_palettes("dialogos", theme = "light", type = "Categorical")
-  dsthemer_palettes("dialogos", theme = "light", type = "Sequential")
-  dsthemer_palettes("dialogos", theme = "light", type = "Divergent")
-
-  dsthemer_palette("dialogos", theme = "light", palette = "main")
-
+  load_dsthemer_json("elconfidencial")
+  dsthemer_palette("elconfidencial", "dark")
+  dsthemer_palette("elconfidencial", theme = "light", palette = "divergening")
 
 
 })
 
-test_that("Get theme", {
-
-  library(parmesan)
-  th <- dsthemer_get("datasketch", "light")
-  parmesan <- parmesan::parmesan_load(system.file("app/parmesan",
-                          package = "dsthemer"))
-  parmesan_pal_colors <- parmesan$palette$inputs[[1]]$input_params$colors
-  th_colors <- th$palette_colors
-  presets <- dsthemer_presets(th)
-  parmesan <- parmesan::parmesan_load(path = system.file("app/parmesan",
-                                                  package = "dsthemer"),
-                                      presets = presets)
-  parmesan_pal_colors <- parmesan$palette$inputs[[1]]$input_params$colors
-  expect_equal(parmesan_pal_colors, th_colors)
-
-})
-
-test_that("Fonts", {
-
-  # TODO
-  # Test all fonts for all themes are available locally
-
-})
-
-test_that("App works", {
-
-  # TODO
-  # Test all parmesan inputs are theme inputs in dsvizopts
-
-})
